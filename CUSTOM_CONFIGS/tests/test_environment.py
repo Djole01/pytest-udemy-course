0832f1e7d75f6ee0f1475b3c54a693b7b6918dda
sdfe <djordje_obradovic33@protonmail.com>
2021-04-08 13:42:15 +0300

    skipping tests, expected failures

--- CUSTOM_CONFIGS/tests/test_environment.py
+++ CUSTOM_CONFIGS/tests/test_environment.py
@@ -1,11 +1,13 @@
+from pytest import mark
 # test command line arg --env
 # custom configs based on dev or qa env
+
+@mark.xfail(reason="example: feature should be deprecated")     # expected failure
 def test_environment_is_qa(app_config):
     base_url = app_config.base_url
     port = app_config.app_port
     assert base_url == 'https://myqa-env.com'
     assert port == 80
-
 
 def test_environment_is_dev(app_config):
     base_url = app_config.base_url
@@ -13,10 +15,15 @@ def test_environment_is_dev(app_config):
     assert base_url == 'https://mydev-env.com'
     assert port == 8080
 
+@mark.skip(reason="Not a staging environment")  # skipping tests
 def test_environment_is_staging(app_config):
     base_url = app_config.base_url
     port = app_config.app_port
     assert base_url == 'staging'
+    
+@mark.skip(reason="broken, we will fix this next sprint")    
+def test_this_needs_work():
+    assert False
     
 # asserts just for trying out test cases
 # normally you would use the base_url depending on the env
